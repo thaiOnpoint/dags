141b7bdd016197c7f94b491f5348f0f516b3e8bb
Thai Pham <101542486+thaiOnpoint@users.noreply.github.com>
2022-04-06 11:18:16 +0700

    Update test

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ Code that goes along with the Airflow tutorial located at:
 https://github.com/apache/incubator-airflow/blob/master/airflow/example_dags/tutorial.py
 """
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-
+import time
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
-    'email': ['airflow@example.com'],
+    'start_date': datetime(2022, 4, 4),
+    'email': ['thai.pham@onpoint.vn'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -21,38 +21,18 @@ default_args = {
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
+def sleep_t():
+    time.sleep(600)
 
 dag = DAG(
     'tutorial', 
     default_args=default_args, 
     schedule_interval=timedelta(days=1))
 
-# t1, t2 and t3 are examples of tasks 
-# created by instantiating operators
-t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
-    dag=dag)
-
-t2 = BashOperator(
+t1 = PythonOperator(
     task_id='sleep',
-    bash_command='sleep 5',
-    retries=3,
+    python_callable=sleep_t,
     dag=dag)
+t1
 
-templated_command = """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-"""
 
-t3 = BashOperator(
-    task_id='templated',
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
-    dag=dag)
-
-t2.set_upstream(t1)
-t3.set_upstream(t1)
